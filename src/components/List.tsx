6f070b69df54b681a800b3592ffa412ef210680c
Omkar Shete <omkaruhs@gmail.com>
2023-12-31 00:25:49 +1300

    add static styling to List and Todo component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,22 @@
+import Todo from "./Todo";
+
 function List() {
-  return <div>List</div>;
+  return (
+    <div className="border-2 border-white min-h-[calc(85%-40px)] max-h-fit rounded-xl p-8 m-4 ">
+      <div className="label text-white flex justify-between items-center px-2">
+        <h1 className="text-5xl font-semibold">List</h1>
+        <button className="text-xl border-2 border-black p-2 rounded-xl">
+          Clear
+        </button>
+      </div>
+      <hr className="my-4 opacity-40" />
+
+      <Todo>My name</Todo>
+      <Todo>My name</Todo>
+      <Todo>My name</Todo>
+      <Todo>My name</Todo>
+    </div>
+  );
 }
 
 export default List;
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,18 @@
-export default function Todo() {
-  return <div></div>;
+type Prop = {
+  children: string;
+};
+
+export default function Todo({ children }: Prop) {
+  return (
+    <div className="flex gap-4 leading-none text-3xl text-white my-5 px-2 justify-start items-center">
+      <input
+        className="size-5"
+        type="checkbox"
+        name="option"
+        id="option"
+        // checked={checked}
+      />
+      <p className="">{children}</p>
+    </div>
+  );
 }
