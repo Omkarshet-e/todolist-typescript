bc2247597a29fa73470401697adb1545beb4e810
Omkar Shete <omkaruhs@gmail.com>
2024-01-06 12:27:01 +1300

    modify styles for responsive design

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -19,16 +19,21 @@ export default function Todo({ todos, setTodos, children }: Prop) {
     <div className="flex gap-4 leading-none text-xl md:text-3xl text-white my-5 px-2 justify-between items-center ">
       <div className="flex items-center justify-start gap-4 leading-none">
         <input
-          className="size-5 cursor-pointer"
+          className="size-5 cursor-pointer grow-0 shrink-0"
           type="checkbox"
           name="option"
           id="option"
           checked={checked}
           onChange={handleClick}
         />
-        <p className={`${checked ? "line-through" : ""}`}>{children}</p>
+        <p
+          className={` grow-[2]
+         ${checked ? "line-through" : ""}`}
+        >
+          {children}
+        </p>
       </div>
-      <div className="btn-remove border-4 border-red-600 p-3 pt-1 rounded-xl items-center">
+      <div className="btn-remove border-2 border-black p-3 pt-1 rounded-xl items-center">
         <button onClick={handleRemoveTodo} className="p-0 m-0">
           x
         </button>
