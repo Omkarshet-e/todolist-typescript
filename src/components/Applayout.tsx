ed8958648e72be4b1e229eda0eedff85d5c81e81
Omkar Shete <omkaruhs@gmail.com>
2024-01-06 12:27:40 +1300

    add local storage

--- src/components/Applayout.tsx
+++ src/components/Applayout.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Inputbar, List } from ".";
 
 export default function Applayout() {
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todosList") as string) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("todosList", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <div className="md:w-3/4 w-full max-w-[1240px] m-10 md:mx-auto">
+    <div className="md:w-3/4 w-full max-w-[1240px] m-10 md:mx-auto max-sm:mx-2">
       <Inputbar setTodos={setTodos} />
       <List todos={todos} setTodos={setTodos} />
     </div>
